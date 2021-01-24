74d35b7fcc639dd3319f14c678263a8b9ed2c49d
Victor <sexsportandrocknroll@gmail.com>
2021-01-24 13:34:46 +0300

    Initial

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+    private Transform target;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // @TODO can be optimized with Coroutines https://docs.unity3d.com/Manual/Coroutines.html
+    private void Update() {
+        if (target) {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    private void FaceTarget() {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    public void MoveToPoint(Vector3 point) {
+        agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget) {
+        agent.stoppingDistance = newTarget.radius * .8f;
+        agent.updateRotation = false;
+        target = newTarget.interactionTransform;
+    }
+
+    public void StopFollowTarget() {
+        agent.stoppingDistance = 0;
+        agent.updateRotation = true;
+        target = null;
+    }
+}
